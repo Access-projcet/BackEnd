283442c4f9e4e24ec0fd52f4882b645f7eef735e
이승렬 <ibban9810@icloud.com>
2023-04-06 18:08:02 +0900

    Refactor : Basic Code Refactoring #1
    1. User 부분 Refactoring

--- src/main/java/com/solver/solver_be/domain/user/dto/UserSearchRequestDto.java
+++ src/main/java/com/solver/solver_be/domain/user/dto/UserSearchRequestDto.java
@@ -4,9 +4,8 @@ import lombok.Getter;
 
 @Getter
 public class UserSearchRequestDto {
-
-    private String name;
-    private String phoneNum;
-    private String email;
-    private String code;
+    private String name;            // Guest/Admin 이름
+    private String phoneNum;        // Guest/Admin 휴대폰 번호
+    private String email;           // Guest/Admin 이메일
+    private String code;            // 이메일 인증코드
 }
--- src/main/java/com/solver/solver_be/domain/user/repository/AdminRepository.java
+++ src/main/java/com/solver/solver_be/domain/user/repository/AdminRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-
     Optional<Admin> findByUserId(String userId);
     Optional<Admin> findByName(String username);
     Admin findAdminByNameAndPhoneNum(String name, String phoneNum);
