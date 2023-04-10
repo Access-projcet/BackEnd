7168e3a1f71a1e2fe5041dd4b42277168219d7a6
이승렬 <ibban9810@icloud.com>
2023-04-11 02:26:05 +0900

    Refactor : Basic Code Refactoring #1
    1. QrCode SMS 전송 Logic

---
+++ src/main/java/com/solver/solver_be/domain/qrCode/dto/QRCodeRequestDto.java
@@ -0,0 +1,10 @@
+package com.solver.solver_be.domain.qrCode.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class QRCodeRequestDto {
+    private String imgUrl;
+}
--- src/main/java/com/solver/solver_be/domain/qrCode/service/QRCodeService.java
+++ src/main/java/com/solver/solver_be/domain/qrCode/service/QRCodeService.java
@@ -1,15 +1,17 @@
 package com.solver.solver_be.domain.qrCode.service;
 
+import com.solver.solver_be.domain.qrCode.dto.QRCodeRequestDto;
 import com.solver.solver_be.domain.qrCode.dto.QRCodeResponseDto;
 import com.solver.solver_be.domain.qrCode.entity.QRCode;
 import com.solver.solver_be.domain.qrCode.repository.QRCodeRepository;
 import com.solver.solver_be.domain.user.entity.Guest;
 import com.solver.solver_be.global.response.GlobalResponseDto;
-import com.solver.solver_be.global.type.ErrorType;
 import com.solver.solver_be.global.type.SuccessType;
+import com.solver.solver_be.global.util.sms.service.SmsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -19,6 +21,7 @@ public class QRCodeService {
     private final QRCodeRepository qrCodeRepository;
 
     // 1. Get Info for QRCode Create
+    @Transactional
     public ResponseEntity<GlobalResponseDto> createQRCode(Guest guest) {
 
         // QRCodeRepo save
@@ -26,4 +29,5 @@ public class QRCodeService {
 
         return ResponseEntity.ok(GlobalResponseDto.of(SuccessType.ACCESS_IN_SUCCESS, QRCodeResponseDto.of(qrCode)));
     }
+
 }
